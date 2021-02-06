486ff868db127c206c186ae7548130f058d6df66
Jake Melville <jakemelville@ymail.com>
2021-02-06 15:07:12 -0500

    added a few comments and tried adding loop. Was unsuccessful with the loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,18 @@ var specialChar = "!@#$%^&*+=/?";
 var password = "";
 
 
+generateBtn.addEventListener("click", passwordCriteria)
+
+generateBtn.onclick = passwordCriteria();
+
 // Write password to the #password input
-function passwordCriteria() {
+function passwordCriteria(){
 
     var confirmLength = parseInt(prompt("Pick a length between 8-128")); //converted answer from string to int
     console.log(confirmLength);
 
     if (isNaN(confirmLength) === true) {
-        alert("Password must be a number.")
+        alert("Password must be a number.") //alert will pop up when the user did not meet the correct criteria for the prompt
         return passwordCriteria(); //if the input is not a number or not within 8-128, the function will restart
     }
     if (confirmLength < 8 || confirmLength > 128) {
@@ -61,7 +65,8 @@ function passwordCriteria() {
 
     window.globalConfirmLength = confirmLength; //redefined as global varaiable so I can use later in code
     window.globalPassword = password; //same as above
-   
+
+
     if (confirmUpper === false && confirmLower === false && confirmNumbers === false && confirmSpecial === false) {
         alert("Must select criteria for password")
         return passwordCriteria();
@@ -69,7 +74,7 @@ function passwordCriteria() {
     return password; //returns one string of all potential characters that can be used in password.
 }
 //ran these console logs to ensure i have the right data types
-console.log(passwordCriteria());
+// passwordCriteria;
 console.log(globalConfirmLength)
 console.log(typeof globalConfirmLength);
 console.log(globalPassword.length);
@@ -80,23 +85,27 @@ console.log(globalPassword.length);
 function passwordContents(){
     var index = Math.floor(Math.random() * globalPassword.length);
     var finalPass = globalPassword[index];
+    console.log(finalPass) //this is our random password
     return finalPass; 
+    
 };
 
+console.log(passwordContents());
 //now taking the above function and looping it using globalConfirmLength. 
 //(which was the answer to the first prompt asking how many characters)
-var combineCharacters = "";
 
+var combineCharacters = ""; //empty variable which I will assign our password string to below
+
+function printText() {
 for (i = 0; i < globalConfirmLength; i++) {
     
     var passwordTextbox = passwordContents();
     combineCharacters = combineCharacters + passwordTextbox;
 } 
+    var myTextArea = document.querySelector("#password"); //using variable textarea I am now changing the textbox in html to the new string stored in combineCharacters
+    myTextArea.innerHTML += combineCharacters;
 
-console.log(combineCharacters)
-console.log(typeof combineCharacters)
-
-//using variable textarea I am now changing the textbox in html to the new string stored in combineCharacters
-var myTextArea = document.querySelector("#password");
-myTextArea.innerHTML += combineCharacters;
-
+}    
+console.log(combineCharacters);
+console.log(typeof combineCharacters);
+printText();
